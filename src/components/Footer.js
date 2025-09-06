2593e416e3537242eda36f5976c0615c19d340fb
njjacobsdev <154025810+njjacobsdev@users.noreply.github.com>
2025-09-06 11:34:33 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© {2024} Intrigue Handman Services LLC</p>
+        <p>© {2024} Intrigue Handyman Services | Greater Milwaukee WI</p>
         <p>
           Phone: <a href="tel:12622390762">(262) 239-0762</a> | 
           Email: <a href="mailto:intrigue.handyman@gmail.com">intrigue.handyman@gmail.com</a>
